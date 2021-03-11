629f45750a0a5c4f776c0aa32bdf3ec48839131a
heuzin <matheusqs.arq@gmail.com>
2021-03-10 23:10:40 -0300

    styles for card list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import './card-list.styles.css';
 
 export const CardList = (props) => (
-    <div>{props.children}</div>
+    <div className='card-list'>{props.children}</div>
 );
