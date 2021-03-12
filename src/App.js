a0ca9079f7df54126416fc64fe5280e8d5613450
heuzin <matheusqs.arq@gmail.com>
2021-03-11 23:29:50 -0300

    handleChange function

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ class App extends React.Component {
     .then(users => this.setState({ monsters: users }))
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render () {
     const { monsters, searchField } = this.state
 
@@ -28,7 +32,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <SearchBox placeholder='search monsters' handleChange={e => this.setState({ searchField: e.target.value})}/>
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange}/>
         <CardList monsters={filteredMonsters}/>
       </div>
     )
